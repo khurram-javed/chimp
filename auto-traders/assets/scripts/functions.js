8ead916bf0c22ef556bf10bc82046111ff601eb2
khurram-javed <khurram.javed.freelancer@gmail.com>
2015-02-19 17:33:05 +0500

    js for Sumo Select

--- auto-traders/assets/scripts/functions.js
+++ auto-traders/assets/scripts/functions.js
@@ -3,4 +3,16 @@
  * --------------------------------------------------------------------------- */
 	jQuery(document).ready(function($) {
 		jQuery(".sub-dropdown").parent("li").addClass("parentIcon");
+
+		//Search Select Add icon
+		jQuery('.SumoSelect').find('i').hide();
+		jQuery('.sidebar-search .SumoSelect').on('click', '.options li', function(e){
+			// var val = jQuery(this).find('label').html();
+			var disabled = jQuery(this).hasClass('disabled');
+			if(!disabled){
+				jQuery(this).parents('.SumoSelect').find('i').show();
+			}else{
+				jQuery(this).parents('.SumoSelect').find('i').hide();
+			}
+		});
 	});
