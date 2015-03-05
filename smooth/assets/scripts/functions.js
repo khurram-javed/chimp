b363f71fb904bec17ff99714051c7712aac568e9
khurram-javed <khurram.javed.freelancer@gmail.com>
2015-03-05 12:46:47 +0500

    Smooth Project package

---
+++ smooth/assets/scripts/functions.js
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------------
+ * Navigation Sub Arrow Function
+ * --------------------------------------------------------------------------- */
+	jQuery(document).ready(function($) {
+		jQuery(".sub-dropdown").parent("li").addClass("parentIcon");
+	});
+
+	/* Flex SLider */
+	$(window).load(function() {
+	  $('.flexslider').flexslider({
+	    animation: "slide"
+	  });
+	});
