8fd0a1fe5d9eeb86c3a61965c6be95b44c597020
khurram-javed <khurram.javed.freelancer@gmail.com>
2015-03-09 11:49:03 +0500

    smooth final

--- smooth/assets/scripts/functions.js
+++ smooth/assets/scripts/functions.js
@@ -11,3 +11,26 @@
 	    animation: "slide"
 	  });
 	});
+
+/* ---------------------------------------------------------------------------
+ * Accordian Toggle Function Function
+ * --------------------------------------------------------------------------- */
+jQuery(document).ready(function(e) {
+	jQuery(".cs-pricetable ul").find('li').after( "<li class='cs-lispacer'></li>" );
+	$(".table-content").hide();
+	jQuery('.cs-pricewrap').eq(0).find('.table-content').show();
+	  jQuery('.accordion-panel').bind('click',function(e){
+	  	e.preventDefault();
+		  if( jQuery(this).parents('.cs-pricewrap').hasClass('up')){
+			jQuery(this).parents('.cs-pricewrap').removeClass('up');
+			jQuery('.table-content,.table-content').slideUp();
+			
+		  }else{
+			jQuery('.cs-pricewrap').removeClass('up');
+			jQuery(this).parents('.cs-pricewrap').addClass('up');
+			jQuery('.cs-pricewrap .table-content,.table-content').slideUp();
+			jQuery('.cs-pricewrap.up .table-content').slideDown();
+		  }
+	  });
+	  
+  });
